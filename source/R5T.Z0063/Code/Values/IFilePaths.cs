0a1ad048b68242a6a80cefeb64a5d3603d724d6e
davidcoats <david@getsafetycone.com>
2023-11-05 11:53:24 -0800

    Interim changes.

--- source/R5T.Z0063/Code/Values/IFilePaths.cs
+++ source/R5T.Z0063/Code/Values/IFilePaths.cs
@@ -13,22 +13,24 @@ namespace R5T.Z0063
     [ValuesMarker]
     public partial interface IFilePaths : IValuesMarker
     {
-        private static Platform.IFilePaths Platform => Z0063.Platform.FilePaths.Instance;
+#pragma warning disable IDE1006 // Naming Styles
+        public Platform.IFilePaths _Platform => Platform.FilePaths.Instance;
+#pragma warning restore IDE1006 // Naming Styles
 
 
-        public ITextFilePath HumanOutputTextFilePath => Platform.HumanOutputTextFilePath.ToTextFilePath();
-        public ILogFilePath LogFilePath => Platform.LogFilePath.ToLogFilePath();
-        public IDataFilePath OutputDataFilePath => Platform.OutputDataFilePath.ToDataFilePath();
+        public ITextFilePath HumanOutputTextFilePath => _Platform.HumanOutputTextFilePath.ToTextFilePath();
+        public ILogFilePath LogFilePath => _Platform.LogFilePath.ToLogFilePath();
+        public IDataFilePath OutputDataFilePath => _Platform.OutputDataFilePath.ToDataFilePath();
 
         /// <inheritdoc cref="Platform.IFilePaths.OutputErrorsTextFilePath"/>
-        public ITextFilePath OutputErrorsTextFilePath => Platform.OutputErrorsTextFilePath.ToTextFilePath();
-        public IHtmlFilePath OutputHtmlFilePath => Platform.OutputHtmlFilePath.ToHtmlFilePath();
-        public IJsonFilePath OutputJsonFilePath => Platform.OutputJsonFilePath.ToJsonFilePath();
-        public ITextFilePath OutputTextFilePath => Platform.OutputTextFilePath.ToTextFilePath();
-        public ITextFilePath OutputTextFilePath_Secondary => Platform.OutputTextFilePath_Secondary.ToTextFilePath();
-        public ITextFilePath OutputTextFilePath_Tertiary => Platform.OutputTextFilePath_Tertiary.ToTextFilePath();
-        public IXmlFilePath OutputXmlFilePath => Platform.OutputXmlFilePath.ToXmlFilePath();
-        public IJsonFilePath RawOutputJsonFilePath => Platform.RawOutputJsonFilePath.ToJsonFilePath();
-        public IJsonFilePath ResultOutputJsonFilePath => Platform.ResultOutputJsonFilePath.ToJsonFilePath();
+        public ITextFilePath OutputErrorsTextFilePath => _Platform.OutputErrorsTextFilePath.ToTextFilePath();
+        public IHtmlFilePath OutputHtmlFilePath => _Platform.OutputHtmlFilePath.ToHtmlFilePath();
+        public IJsonFilePath OutputJsonFilePath => _Platform.OutputJsonFilePath.ToJsonFilePath();
+        public ITextFilePath OutputTextFilePath => _Platform.OutputTextFilePath.ToTextFilePath();
+        public ITextFilePath OutputTextFilePath_Secondary => _Platform.OutputTextFilePath_Secondary.ToTextFilePath();
+        public ITextFilePath OutputTextFilePath_Tertiary => _Platform.OutputTextFilePath_Tertiary.ToTextFilePath();
+        public IXmlFilePath OutputXmlFilePath => _Platform.OutputXmlFilePath.ToXmlFilePath();
+        public IJsonFilePath RawOutputJsonFilePath => _Platform.RawOutputJsonFilePath.ToJsonFilePath();
+        public IJsonFilePath ResultOutputJsonFilePath => _Platform.ResultOutputJsonFilePath.ToJsonFilePath();
     }
 }
